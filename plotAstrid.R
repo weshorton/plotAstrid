4c76c715024ca955cc88a6e9c45701d776b60db3
weshorton <wrhorton592@gmail.com>
2021-07-28 10:57:33 -0800

    Cleaned up functions and documentation. Added a few examples. Put in a to-do list of next items.

--- plotAstrid.R
+++ plotAstrid.R
@@ -4,40 +4,26 @@
 ### PLOT ASTRID ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ###################
 
-### This script is the base for plotting various forms of the astrid geometric thing.
+### This script contains various functions used for plotting different permutations of the
+### astrid geometric shape.
 
-### The main function requires an x-axis and a y-axis of either the same length, or a whole-number ratio of lengths
-  ### If one number is given, will be same for both, if two, then will do ratio
-### Can have from 1:n of these axis pairs
-### The various pairs must exist on the regular coordinate plane
-  ### Each axis is defined as a line between the origin and some given point.
-  ### Needs to be fully customizable and also automatic.
-  ### Default will be equidistant angles with positive y-axis as main reference
-    ### Two axes - quadrant I - maybe just omit this since it's not regular
-    ### Three axes - y-axis from origin to length, then other two axes in quadrants 3 and 4 b/c 120* between them
-    ### Four axes - full x and y axis, b/c 90* between them
-    ### Five : 72; Six : 60; Seven : 51.428; Eight : 45
-    ### Populated by:
-      ### Everything is referenced from positive y-axis going counter-clockwise
-        ### Quadrants are one less than the "standard", so the "top-left" quadrant (i.e. -x,y) is I (instead of the usual II)
-        ### To determine the (x,y) coordinate of the point that will make the line extending from the origin that will make the
-        ### "new" axis limit, take the sine and cosine of the appropriate angle. The sign of the x and y are determined by quadrant.
-        ### I: x = -sin(theta); y = cos(theta)
-        ### II: x = -cos(theta); y = -sin(theta)
-        ### III: x = sin(theta); y = -cos(theta)
-        ### IV: x = cos(theta); y = sin(theta)
-        ### Quadrant is determined by how many times you must subtract 90 from your starting angle to get an angle less than 90
-        ### Quadrant one is comprised of angles <90, so you never subtract
-        ### II is comprised of 91-180, so you have to subtract 90 once
-        ### must subtract twice for III and three times for IV
+### Using a regular coordinate plane. Plot new "axes" based on regular geometry and then plot
+### "webs" of lines between these new axes based upon a given density.
 
-##############################
-### DEPENDENCIES/FUNCTIONS ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-####################
+### Positive y-axis is always the first/reference axis.
 
-suppressMessages(library(ggplot2))
-suppressMessages(library(data.table))
-suppressMessages(library(wrh.rUtils))
+
+### TO DO ###
+
+### 1. NEED TO ADD SAVE/FILE ARGUMENTS TO PLOTASTRID
+### 2. REMOVE DATA MANIPULATION FROM PLOTWEB AND PUT INTO GETWEBPOINTS
+### 3. ORGANIZE PLOTTING OF AXES AND WEBS TOGETHER IN PLOTWEB (no plotting in plotAstrid except the call to plotWeb)
+### 4. ADD MORE PLOTTING FUNCTIONALITY
+### 5. OUTPUT POINTS TO OBJECT FOR LATER PLOTTING
+
+#################
+### FUNCTIONS ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#################
 
 pointFinder <- function(fullAngle_v,trigAngle_v, segmentLength_v) {
   #' Point Finder
@@ -47,6 +33,7 @@ pointFinder <- function(fullAngle_v,trigAngle_v, segmentLength_v) {
   #' the fullAngle until the remainder is less than 90.
   #' @param segmentLength_v The distance of the point from the origin, i.e. the length of the line segment. Used to find the x and y.
   #' @return Returns vector of format c(x,y).
+  #' @export
   
   ## Convert trigAngle_v from degrees to radians
   trigAngle_v <- trigAngle_v * pi / 180
@@ -66,10 +53,21 @@ pointFinder <- function(fullAngle_v,trigAngle_v, segmentLength_v) {
   return(point_v)
 } # pointFinder
 
-getWebPoints <- function(lengthAxes_v, density_v, fullAngles_v, trigAngles_v) {
+getWebPoints <- function(lengthAxes_v, density_v = NULL, fullAngles_v, trigAngles_v) {
+  #' Get Web Points
+  #' @description Get all of the points for all of the line segments that will make the web
+  #' @param lengthAxes_v Numeric vector whose values are the length of each axis.
+  #' @param density_v Numeric vector representing the number of lines that will be in the web. Default
+  #' is to be the same as the length of the axis.
+  #' @param fullAngles_v Numeric vector containing each axis' angle relative to the positive y-axis
+  #' @param trigAngles_v Numeric vector containing each axis' "trigonometric angle", found by subtracting
+  #' 90* until acute angle is found, this angle is used with sine/cosine properties to get x,y coordinates
+  #' @return list of lists. Each main list element corresponds to one of the main axes. Its child is a list
+  #' of x,y coordinates for the points along that line that the web will intersect.
+  #' @export
   
-  ### Use the density and axis length to get the "step" of the web and the sequence of steps
-  ### along the axis
+  ### Use the density and axis length to get the "step" of the web 
+  ### and the sequence of steps along the axis
   step_v <- lengthAxes_v / density_v
   sequences_lsv <- mapply(function(x,y) seq(x, y, x), step_v, lengthAxes_v, SIMPLIFY = F)
   
@@ -91,13 +89,24 @@ getWebPoints <- function(lengthAxes_v, density_v, fullAngles_v, trigAngles_v) {
 } # getWebPoints
 
 plotWeb <- function(webPoints_lslsv, save_v = F, outDir_v = "./", outName_v = "astrid.pdf") {
+  #' Plot Web
+  #' @description Use web points to add line segments to existing plot.
+  #' This should not be a function. Need to take the first-second point construction and add it to
+  #' getWebPoints I think so that I can save it as an object. All the plotting should be done together.
+  #' @param webPoints_lslsv list with one element for each axis that contains a list of x,y coordinates for the web.
+  #' @param save_v logical. TRUE - save using outDir_v and outName_v; FALSE - print to console
+  #' @param outDir_v directory to save file. Default to current directory
+  #' @param outName_v filename. Default to astrid.pdf
+  #' @return plot
+  #' @export
   
   ### Open device
   if (save_v) {
     pdf(file.path(outDir_v, outName_v))
   }
   
-  ### Now for each set of points, use the subsequent set to make line segments. For the last set, use the first set.
+  ### For each set of points, use the subsequent set to make line segments. 
+  ### For the last set, use the first set.
   for (i in 1:length(webPoints_lslsv)) {
     
     ### Get first set
@@ -128,48 +137,17 @@ plotWeb <- function(webPoints_lslsv, save_v = F, outDir_v = "./", outName_v = "a
   if (save_v) dev.off()
 } # plotWeb
 
-
-#################
-### ARGUMENTS ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#################
-
-### numAxes - numerical vector of how many axes and correspondigly "webs" you want, with 3 being the minimum.
-### length - the length of the line that each axis is relative to a standard scale
-### density - the number of lines in the web, default is to be same as length
-
-# ### Sparse 5-point
-# numAxes_v <- 5
-# lengthAxes_v <- rep(6,5)
-# density_v <- 6
-# 
-# ### Dense 5-point
-# numAxes_v <- 5
-# lengthAxes_v <- rep(6,5)
-# density_v <- 12
-
-### Dense 4-point
-numAxes_v <- 4
-lengthAxes_v <- rep(10, 4)
-density_v <- 20
-
-# ### Dense 8-point
-# numAxes_v <- 8
-# lengthAxes_v <- rep(10,8)
-# density_v <- 20
-
 plotAstrid <- function(numAxes_v = 4, lengthAxes_v = rep(10,4), density_v = 20) {
+  #' Plot Astrid
+  #' @description Create various permutations on the astrid geometric shape
+  #' @param numAxes_v Numeric value indicating the number of axes to plot webs between. 3 is minimum.
+  #' @param lengthAxes_v Numeric vector indicating the length of each axis. length(lengthAxes_v) = numAxes_v
+  #' @param density_v Numeric vector indicating the number of lines in each web
+  #' @return astrid data and also a plot.
   
-  ###
-  ### Get main axis information
-  ###
-  
-  ### Get coordinates for each of the axes
+  ### Get the full and trig angles of each axis
   baseAngle_v <- 360 / numAxes_v
-  
-  ### Get the full angles
   fullAngles_v <- sapply(1:(numAxes_v-1), function(x) baseAngle_v * x)
-  
-  ### Get the trig angles
   trigAngles_v <- sapply(fullAngles_v, function(x) {
     while(x > 90) x <- x - 90
     return(x)
@@ -195,58 +173,21 @@ plotAstrid <- function(numAxes_v = 4, lengthAxes_v = rep(10,4), density_v = 20) 
   
 } # plotAstrid
 
+################
+### EXAMPLES ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+################
+
+### Dense 4-point
 plotAstrid()
 
-#################
-### MAKE AXES ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#################
+### Dense 8-point
+plotAstrid(numAxes_v = 8, lengthAxes_v = rep(10,8), density_v = 20)
 
-# ### Get coordinates for each of the axes
-# baseAngle_v <- 360 / numAxes_v
-# 
-# ### Get the full angles
-# fullAngles_v <- sapply(1:(numAxes_v-1), function(x) baseAngle_v * x)
-# 
-# ### Get the trig angles
-# trigAngles_v <- sapply(fullAngles_v, function(x) {
-#   while(x > 90) x <- x - 90
-#   return(x)
-# })
-# 
-# ### Get the points for the axis segments
-# axisPoints_lsv <- mapply(pointFinder, fullAngles_v, trigAngles_v, lengthAxes_v[2:numAxes_v], SIMPLIFY = F)
-# 
-# ### Prepend the first axis
-# axisPoints_lsv <- rlist::list.prepend(axisPoints_lsv, c(0,lengthAxes_v[1]))
-# 
-# ### Test plot
-# plot(1, type = "n", xlim=c(-10,10), ylim = c(-10,10))
-# for (i in 1:length(axisPoints_lsv)) {
-#   points(x = c(0, axisPoints_lsv[[i]][1]), y = c(0,axisPoints_lsv[[i]][2]), type = 'l')
-# }
+### Sparse 8-point
+plotAstrid(numAxes_v = 8, lengthAxes_v = rep(10,8), density_v = 10)
 
-#################
-### MAKE WEBS ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#################
-
-# ### Have to draw one web for each axis
-# ### As many lines as there is density.
-# ### Start at the positive y axis and then go counter-clockwise.
-#   ### Point 1 of axis1 goes to pointAxisLength on axis2
-#   ### Point 2 of axis1 goes to (point (AxisLength-1)) on axis2
-#   ### etc.
-# ### How to find the points? Same trig thing, but instead of the length being the axis length, it will be a division of it.
-# ### These are the things that need to be lined up in inverse of each other for the web lines.
-# ### Will be dependent on the density.
-# 
-# ### In this example, there will be 5 sets of 12 lines
-# 
-# ### For the first one, each point will be c(0,len/dens), c(0,(len/dens)-1) etc.
-# ### Second point will be trigValue*len, trigValue*len-1, etc.
-# ### Get web points
-# webPoints_lslsv <- getWebPoints(lengthAxes_v, density_v, fullAngles_v, trigAngles_v)
-# ### Plot them
-# plotWeb(webPoints_lslsv)
+### Dense 5-point
+plotAstrid(numAxes_v = 5, lengthAxes_v = rep(10,5), density_v = 20)
 
 
 
