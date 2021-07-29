80d05187caa3b04e82e1984f8bf5e0551254d004
weshorton <wrhorton592@gmail.com>
2021-07-28 19:13:05 -0800

    Separated the data manipulation and plotting code into their own functions. Next step is adding plotting functionality

--- plotAstrid.R
+++ plotAstrid.R
@@ -16,14 +16,19 @@
 ### TO DO ###
 
 ### 1. NEED TO ADD SAVE/FILE ARGUMENTS TO PLOTASTRID
-### 2. REMOVE DATA MANIPULATION FROM PLOTWEB AND PUT INTO GETWEBPOINTS
-### 3. ORGANIZE PLOTTING OF AXES AND WEBS TOGETHER IN PLOTWEB (no plotting in plotAstrid except the call to plotWeb)
 ### 4. ADD MORE PLOTTING FUNCTIONALITY
-### 5. OUTPUT POINTS TO OBJECT FOR LATER PLOTTING
 
 #################
 ### FUNCTIONS ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #################
+
+### Arguments for walkthrough
+numAxes_v <- 5
+lengthAxes_v <- rep(10,5)
+density_v <- 20
+save_v <- F
+outDir_v <- "./"
+outName_v <- "astrid.pdf"
 
 pointFinder <- function(fullAngle_v,trigAngle_v, segmentLength_v) {
   #' Point Finder
@@ -68,12 +73,9 @@ getWebPoints <- function(lengthAxes_v, density_v = NULL, fullAngles_v, trigAngle
   
   ### Use the density and axis length to get the "step" of the web 
   ### and the sequence of steps along the axis
+  if (is.null(density_v)) density_v <- lengthAxes_v
   step_v <- lengthAxes_v / density_v
   sequences_lsv <- mapply(function(x,y) seq(x, y, x), step_v, lengthAxes_v, SIMPLIFY = F)
-  
-  ### Remove first list element (always the positive y-axis) 
-  firstPoints_lsv <- Map(c, rep(0,length(sequences_lsv[[1]])), sequences_lsv[[1]])
-  sequences_lsv[[1]] <- NULL
   
   ### Use pointFinder to get points for the other axes
   webPoints_lslsv <- lapply(1:length(trigAngles_v), function(x) {
@@ -82,96 +84,118 @@ getWebPoints <- function(lengthAxes_v, density_v = NULL, fullAngles_v, trigAngle
     }, simplify = F)
   })
   
-  ### Add first points back
-  ### Do I need to do this? Can i just plug it in with the angle of 0?
-  webPoints_lslsv <- rlist::list.prepend(webPoints_lslsv, firstPoints_lsv)
-  
-} # getWebPoints
-
-plotWeb <- function(webPoints_lslsv, save_v = F, outDir_v = "./", outName_v = "astrid.pdf") {
-  #' Plot Web
-  #' @description Use web points to add line segments to existing plot.
-  #' This should not be a function. Need to take the first-second point construction and add it to
-  #' getWebPoints I think so that I can save it as an object. All the plotting should be done together.
-  #' @param webPoints_lslsv list with one element for each axis that contains a list of x,y coordinates for the web.
-  #' @param save_v logical. TRUE - save using outDir_v and outName_v; FALSE - print to console
-  #' @param outDir_v directory to save file. Default to current directory
-  #' @param outName_v filename. Default to astrid.pdf
-  #' @return plot
-  #' @export
-  
-  ### Open device
-  if (save_v) {
-    pdf(file.path(outDir_v, outName_v))
-  }
-  
   ### For each set of points, use the subsequent set to make line segments. 
   ### For the last set, use the first set.
+  webPoints_lsdt <- list()
   for (i in 1:length(webPoints_lslsv)) {
     
     ### Get first set
-    currFirstPoints_lsv <- webPoints_lslsv[[i]]
+    currFirstPoints_dt <- as.data.table(t(as.data.table(webPoints_lslsv[[i]])))
+    colnames(currFirstPoints_dt) <- c("x", "y")
     
     ### Get second set
     if (i == length(webPoints_lslsv)) {
-      currSecondPoints_lsv <- webPoints_lslsv[[1]]
+      currSecondPoints_dt <- as.data.table(t(as.data.table(webPoints_lslsv[[1]])))
     } else {
-      currSecondPoints_lsv <- webPoints_lslsv[[i+1]]
+      currSecondPoints_dt <- as.data.table(t(as.data.table(webPoints_lslsv[[i+1]])))
     }
     
     ### Reverse second set
-    currSecondPoints_lsv <- rev(currSecondPoints_lsv)
+    currSecondPoints_dt <- currSecondPoints_dt[currSecondPoints_dt[,.N]:1,]
+    colnames(currSecondPoints_dt) <- c("x","y")
     
-    ### Plot
-    if (save_v) {
-      pdf(file.path(outDir_v, outName_v))
-    }
-    for (j in 1:length(currFirstPoints_lsv)) {
-      points(x = c(currFirstPoints_lsv[[j]][1], currSecondPoints_lsv[[j]][1]),
-             y = c(currFirstPoints_lsv[[j]][2], currSecondPoints_lsv[[j]][2]),
-             type = 'l')
-    } # for j
+    ### Combine into one data.table.
+    currLineSegment_dt <- cbind(currFirstPoints_dt,currSecondPoints_dt)
+    colnames(currLineSegment_dt) <- c("x1", "y1", "x2", "y2")
+    
+    ### Add to output
+    webPoints_lsdt[[i]] <- currLineSegment_dt
+
   } # for i
   
-  ### Close device
-  if (save_v) dev.off()
-} # plotWeb
+  ### Return
+  return(webPoints_lsdt)
+  
+} # getWebPoints
 
-plotAstrid <- function(numAxes_v = 4, lengthAxes_v = rep(10,4), density_v = 20) {
+plotAstrid <- function(axisPoints_lsv,
+                    webPoints_lsdt) {
   #' Plot Astrid
+  #' @description Plot axes and webs of astrid.
+  #' @param axisPoints_lsv list of (x,y) vectors that combine with origin to create line segments for axes of astrid
+  #' @param webPoints_lsdt list with one element for each axis that contains a dt of x,y coordinates for the web.
+  #' @return plot
+  #' @export
+  
+  ### Base plot and axes
+  plot(1, type = "n", xlim=c(-10,10), ylim = c(-10,10))
+  for (i in 1:length(axisPoints_lsv)) {
+    points(x = c(0, axisPoints_lsv[[i]][1]), y = c(0,axisPoints_lsv[[i]][2]), type = 'l')
+  } # for i
+
+  ### Iterate over list of webs
+  for (i in 1:length(webPoints_lsdt)) {
+    ### Get x,y coordinates for current web
+    currLineSegment_dt <- webPoints_lsdt[[i]]
+    ### Iterate over rows of x,y coords
+    for (j in 1:currLineSegment_dt[,.N]) {
+      ### Add to plot.
+      points(x = c(currLineSegment_dt$x1[j], currLineSegment_dt$x2[j]),
+             y = c(currLineSegment_dt$y1[j], currLineSegment_dt$y2[j]),
+             type = "l")
+    }  # for j
+  } # for i
+} # plotAstrid
+
+astrid <- function(numAxes_v = 4, lengthAxes_v = rep(10,4), density_v = 20, save_v = F,
+                       outDir_v = "./", outName_v = "astrid.pdf") {
+  #' Astrid
   #' @description Create various permutations on the astrid geometric shape
   #' @param numAxes_v Numeric value indicating the number of axes to plot webs between. 3 is minimum.
   #' @param lengthAxes_v Numeric vector indicating the length of each axis. length(lengthAxes_v) = numAxes_v
   #' @param density_v Numeric vector indicating the number of lines in each web
+  #' @param save_v logical. TRUE - save using outDir_v and outName_v; FALSE - print to console
+  #' @param outDir_v directory to save file. Default to current directory
+  #' @param outName_v filename. Default to numAxes_v_lengthAxes_v[1]_density_v_astrid.pdf
   #' @return astrid data and also a plot.
+  
+  ### Construct output name
+  if (outName_v == "astrid.pdf") {
+    outName_v <- paste(numAxes_v, lengthAxes_v[1], density_v, outName_v, sep = "_")
+  }
   
   ### Get the full and trig angles of each axis
   baseAngle_v <- 360 / numAxes_v
-  fullAngles_v <- sapply(1:(numAxes_v-1), function(x) baseAngle_v * x)
+  fullAngles_v <- c(0, sapply(1:(numAxes_v-1), function(x) baseAngle_v * x))
   trigAngles_v <- sapply(fullAngles_v, function(x) {
     while(x > 90) x <- x - 90
     return(x)
   })
   
   ### Get the points for the axis segments
-  axisPoints_lsv <- mapply(pointFinder, fullAngles_v, trigAngles_v, lengthAxes_v[2:numAxes_v], SIMPLIFY = F)
-  
-  ### Prepend the first axis (don't do the trig on it b/c it's always 1:1 ratio on positive y-axis)
-  axisPoints_lsv <- rlist::list.prepend(axisPoints_lsv, c(0,lengthAxes_v[1]))
+  axisPoints_lsv <- mapply(pointFinder, fullAngles_v, trigAngles_v, lengthAxes_v, SIMPLIFY = F)
   
   ### Get web points
-  webPoints_lslsv <- getWebPoints(lengthAxes_v, density_v, fullAngles_v, trigAngles_v)
+  webPoints_lsdt <- getWebPoints(lengthAxes_v, density_v, fullAngles_v, trigAngles_v)
   
-  ### Plot axes
-  plot(1, type = "n", xlim=c(-10,10), ylim = c(-10,10))
-  for (i in 1:length(axisPoints_lsv)) {
-    points(x = c(0, axisPoints_lsv[[i]][1]), y = c(0,axisPoints_lsv[[i]][2]), type = 'l')
+  ### Open device
+  if (save_v) {
+    pdf(file.path(outDir_v, outName_v))
   }
   
-  ### Plot webs
-  plotWeb(webPoints_lslsv)
+  ### Plot astrid
+  plotAstrid(axisPoints_lsv,
+             webPoints_lsdt)
   
-} # plotAstrid
+  ### Close device
+  if (save_v) dev.off()
+  
+  ### Return data
+  out_ls <- list("axisPoints" = axisPoints_lsv,
+                 "webPoints" = webPoints_lsdt)
+  return(out_ls)
+  
+} # astrid
 
 ################
 ### EXAMPLES ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
